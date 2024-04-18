d22d6e4afbfc5a2acbe6d2ee5ec3a2570455b2dc
Guilherme Rocha <111608183+Asteeeeee@users.noreply.github.com>
2024-04-18 20:50:37 -0300

    Criaçao do card com estilizaçao e HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      Hello, world!
+      <div className="card">
+        <h2>Rich Sanches</h2>
+        <img src="htt/ ps://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
